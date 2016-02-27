09fe7be9f082ba6930c9ca329751753ad8bad94a
h1k0n <ybzjyhk@gmail.com>
2016-02-27 15:34:52 +0800

    different between fmt.Print and fmt.Println

---
+++ fmt_Print_Println.go
@@ -0,0 +1,12 @@
+//Go语言-云动力
+//fmt包中微小的区别，感叹作者的细心
+package main
+
+import "fmt"
+
+func main() {
+	var i = 42
+	var s = "answer"
+	fmt.Print(s, "is", i, 3.14, '\n', "\n")   //两个数字间自动空一格
+    fmt.Println(s,"is",i,"\n",3.14,'\n')  //遇到”，“空一格，自动换行
+}
