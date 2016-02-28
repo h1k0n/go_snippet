bf39556cee0bcdd1892163b3d344567be36463c5
h1k0n <ybzjyhk@gmail.com>
2016-02-28 16:36:22 +0800

    Create an example about nil slice
    from the go programming language

---
+++ nil_slice.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	s :=[]int(nil) //var s []int
+	t :=[]int{} //t := make([]int,3)[3:]
+	fmt.Println("s ==", s," t == ",t)
+	fmt.Println("len(s)=",len(s),"len(t)=",len(t))
+	fmt.Println("cap(s)=",cap(s),"cap(t)=",cap(t))
+	fmt.Println("s==nil ?",s==nil,"t==nil ?",t==nil)
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("s[%d] == %d\n", i, s[i])
+	}
+}
