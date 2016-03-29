e431e434ca45dce2f849abb539a295d86974385c
h1k0n <ybzjyhk@gmail.com>
2016-03-29 22:38:34 +0800

    add comment in this snippet

--- close_channel_do_not_block_receive.go
+++ close_channel_do_not_block_receive.go
@@ -7,7 +7,8 @@ func main() {
     go func() {
         fmt.Println("do not block")
         close(ch)
-    }()
-    <-ch
+    }()    //这个goroutine不会block住，因为没有send或receive操作
+    <-ch   //即便close了也不会阻塞
+     fmt.Println("main")
 }
         
