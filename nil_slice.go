401843ff1ce4024b6e6ad163ea67f88f38bbbe9b
h1k0n <ybzjyhk@gmail.com>
2016-04-03 01:26:29 +0800

    Update nil_slice.go

--- nil_slice.go
+++ nil_slice.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	s :=[]int(nil) //var s []int
-	t :=[]int{} //t := make([]int,3)[3:]
+	t :=[]int{} //t := make([]int,3)[3:] t:=make([]int,0
 	fmt.Println("s ==", s," t == ",t)
 	fmt.Println("len(s)=",len(s),"len(t)=",len(t))
 	fmt.Println("cap(s)=",cap(s),"cap(t)=",cap(t))
