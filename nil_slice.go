7ee2b295ac4484ff7cfaff79a27aa7682b870e0e
h1k0n <ybzjyhk@gmail.com>
2016-04-03 01:33:08 +0800

    Update nil_slice.go
    add comment, nil slice and empty slice's usage

--- nil_slice.go
+++ nil_slice.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	s :=[]int(nil) //var s []int
-	t :=[]int{} //t := make([]int,3)[3:] t:=make([]int,0
+	s :=[]int(nil) //var s []int //usually use in function []int when exception happen,return nill slice
+	t :=[]int{} //t := make([]int,3)[3:] t:=make([]int,0  //represent empty data set, such as select return 
 	fmt.Println("s ==", s," t == ",t)
 	fmt.Println("len(s)=",len(s),"len(t)=",len(t))
 	fmt.Println("cap(s)=",cap(s),"cap(t)=",cap(t))
